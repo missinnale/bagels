c21f266b428c83b2ad18e045ff9731335ff154cb
Elan Moyal <emoyal@mediamath.com>
2015-08-05 18:53:24 -0400

    There are bagels!!!

--- nom.py
+++ nom.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+def openTmp():
+    bagelTxt = open("./tmp/aretherebagels.txt", "r")
+    bagelInfo = bagelTxt.read()
+    bagelArray = bagelInfo.split(' ')
+    if len(bagelArray) > 1:
+        print("Someone messed with the account")
+        return
+    bagelArray = bagelArray[0].split("\n")
+    bagels = bagelArray[0].lower()
+    print(bagels)
+    if bagels == "yes":
+        aretherebagels = True
+    elif bagels == "no":
+        aretherebagels = False
+    else:
+        aretherebagels = None
+    return aretherebagels
+
 @app.route('/')
-def hello_world():
-    return render_template('nobagels.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+def bagels():
+    aretherebagels = openTmp()
+    print(aretherebagels)
+    if aretherebagels:
+        return render_template('bagels.html')
+    else:
+        return render_template('nobagels.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
