7b504bc34ebfcefdd38af4bec099b5de3da79b3a
Elan Moyal <emoyal@mediamath.com>
2015-08-05 19:41:06 -0400

    Let there be bagels!!

--- nom.py
+++ nom.py
@@ -1,28 +1,40 @@
+import os
+import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 def openTmp():
-    bagelTxt = open("./tmp/aretherebagels.txt", "r")
+    try:
+        bagelTxt = open("./tmp/aretherebagels.txt", "r")
+    except OSError:
+        return False
     bagelInfo = bagelTxt.read()
+    bagelTxt.close()
     bagelArray = bagelInfo.split(' ')
     if len(bagelArray) > 1:
         print("Someone messed with the account")
         return
     bagelArray = bagelArray[0].split("\n")
     bagels = bagelArray[0].lower()
-    print(bagels)
     if bagels == "yes":
-        aretherebagels = True
-    elif bagels == "no":
-        aretherebagels = False
-    else:
-        aretherebagels = None
-    return aretherebagels
+        return True
+    return False
+
+
+def deleteTmp():
+    mtime = os.path.getmtime("./tmp/aretherebagels.txt")
+    date = datetime.datetime.fromtimestamp(mtime)
+    now = datetime.datetime.now()
+    elapsedtime = now - date
+    if elapsedtime.days > 0 or elapsedtime.seconds > 7200:
+        os.remove("./tmp/aretherebagels.txt")
+
 
 @app.route('/')
 def bagels():
     aretherebagels = openTmp()
-    print(aretherebagels)
+    deleteTmp()
     if aretherebagels:
         return render_template('bagels.html')
     else:
